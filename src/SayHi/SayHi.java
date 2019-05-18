42c4a8b5bace85ac01557fc79fe966008417c2fa
Woojung <son15005@byui.edu>
2019-05-18 15:55:38 -0600

    woo commits

--- src/SayHi/SayHi.java
+++ src/SayHi/SayHi.java
@@ -5,3 +5,9 @@ public class SayHi {
         System.out.println("Hello, my name is Keith Tung.");
     }
 }
+
+public class SayHi {
+    public static void main(String[] args) {
+        System.out.println("Hello, Keith im Woo. We are Asian :)");
+    }
+}
