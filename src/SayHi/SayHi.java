809650e3cc47e0872fd8268dedcfc037efada385
Your Name <sy17001@byui.edu>
2019-05-18 16:00:07 -0600

    David change commit

--- src/SayHi/SayHi.java
+++ src/SayHi/SayHi.java
@@ -3,6 +3,7 @@ package SayHi;
 public class SayHi {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Keith Tung.");
+        System.out.println("Hello, my name is David and I like money.");
     }
 }
 
